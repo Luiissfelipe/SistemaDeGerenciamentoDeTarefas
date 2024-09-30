2db14fd413f74332859d9f86e955755068230a5c
Luís Felipe Morais <149626850+Luiissfelipe@users.noreply.github.com>
2024-09-30 15:42:23 -0300

    criação do menu

--- src/gerenciadorDeTarefas/Main.java
+++ src/gerenciadorDeTarefas/Main.java
@@ -1,7 +1,50 @@
 package gerenciadorDeTarefas;
 
+import java.util.Scanner;
+
 public class Main {
+    //Menu do gerenciador de tarefas
+    public static void mostrarMenu() {
+        System.out.print("""
+                
+                GERENCIADOR DE TAREFAS
+                
+                [1] - Adicionar tarefa
+                [2] - Remover tarefa
+                [3] - Mudar status da tarefa
+                [4] - Listar tarefas
+                [5] - Salvar e sair
+                
+                Digite a opção desejada:
+                """);
+    }
+
     public static void main(String[] args) {
-        
+        //Declaração das variáveis
+        Scanner input = new Scanner(System.in);
+        int opcao;
+
+        //Loop para executar o código até que seja solicitado o fim
+        do {
+
+            mostrarMenu(); //Chamando o método para mostrar o menu
+            opcao = input.nextInt(); // Lendo a opção escolhida
+
+            switch (opcao) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                default:
+                    System.out.println("\nVocê digitou uma opção invalida!");
+            }
+        } while (opcao != 5);
+
     }
 }
